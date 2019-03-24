720ce318082013614a12dbb77f60899367ea0fcc
jansona <alelipona121@gmail.com>
2019-03-24 13:41:44 +0800

    practice 0

--- lex.yy.c
+++ lex.yy.c
@@ -282,29 +282,30 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 1
-#define YY_END_OF_BUFFER 2
-static yyconst short int yy_accept[6] =
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
+static yyconst short int yy_accept[14] =
     {   0,
-        0,    0,    2,    1,    0
+        0,    0,    5,    4,    2,    1,    1,    3,    2,    1,
+        1,    3,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    3,    1,    1,    1,    1,    4,    1,
+        1,    1,    1,    1,    1,    4,    1,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    1,    1,    1,
+        1,    1,    1,    1,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,29 +322,33 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[2] =
+static yyconst int yy_meta[6] =
     {   0,
-        1
+        1,    2,    3,    4,    5
     } ;
 
-static yyconst short int yy_base[7] =
+static yyconst short int yy_base[17] =
     {   0,
-        0,    0,    2,    3,    3,    0
+        0,    0,   14,   15,    0,    4,    0,    0,    0,    0,
+        0,    0,   15,    7,    8,    8
     } ;
 
-static yyconst short int yy_def[7] =
+static yyconst short int yy_def[17] =
     {   0,
-        6,    6,    5,    5,    0,    5
+       13,    1,   13,   13,   14,   13,   15,   16,   14,    6,
+       15,   16,    0,   13,   13,   13
     } ;
 
-static yyconst short int yy_nxt[5] =
+static yyconst short int yy_nxt[21] =
     {   0,
-        4,    5,    3,    5
+        4,    5,    6,    7,    8,    9,   10,   11,    9,    9,
+       11,   11,   12,   13,    3,   13,   13,   13,   13,   13
     } ;
 
-static yyconst short int yy_chk[5] =
+static yyconst short int yy_chk[21] =
     {   0,
-        6,    3,    5,    5
+        1,    1,    1,    1,    1,    6,    6,    6,   14,   14,
+       15,   15,   16,    3,   13,   13,   13,   13,   13,   13
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -360,8 +365,8 @@ char *yytext;
 #line 1 ".\\lex.l"
 #define INITIAL 0
 #line 2 ".\\lex.l"
-int yywrap(void);
-#line 365 "lex.yy.c"
+	int wordCount = 0;
+#line 370 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -512,9 +517,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 4 ".\\lex.l"
+#line 9 ".\\lex.l"
 
-#line 518 "lex.yy.c"
+#line 523 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -565,13 +570,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 6 )
+				if ( yy_current_state >= 14 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 3 );
+		while ( yy_base[yy_current_state] != 15 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -599,10 +604,25 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 5 ".\\lex.l"
+#line 10 ".\\lex.l"
+{ wordCount++; /*increase the word count by one*/ }
+	YY_BREAK
+case 2:
+YY_RULE_SETUP
+#line 11 ".\\lex.l"
+{ /* donothing*/ }
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 12 ".\\lex.l"
+{ /* one maywant to add some processing here*/ }
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 13 ".\\lex.l"
 ECHO;
 	YY_BREAK
-#line 606 "lex.yy.c"
+#line 626 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -894,7 +914,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 6 )
+			if ( yy_current_state >= 14 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -929,11 +949,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 6 )
+		if ( yy_current_state >= 14 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 5);
+	yy_is_jam = (yy_current_state == 13);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1488,9 +1508,14 @@ int main()
 	return 0;
 	}
 #endif
-#line 5 ".\\lex.l"
+#line 13 ".\\lex.l"
 
-int yywrap(void)
+void main()
+{
+	yylex(); /* start the analysis*/
+	printf(" No of words:%d\n", wordCount);
+}
+int yywrap()
 {
 	return 1;
 }
